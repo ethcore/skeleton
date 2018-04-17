553b787a910ecd44873fc390d96aaee6ff2339d8
3for <30716372+3for@users.noreply.github.com>
2018-04-17 17:09:09 +0800

    Resolve webpack error
    Error info:
    Invalid configuration object. Webpack has been initialised using a configuration object that does not match the API schema.
     - configuration.module has an unknown property 'loaders'. These properties are valid:
       object { exprContextCritical?, exprContextRecursive?, exprContextRegExp?, exprContextRequest?, noParse?, rules?, defaultRules?, unknownContextCritical?, unknownContextRecursive?, unknownContextRegExp?, unknownContextRequest?, unsafeCache?, wrappedContextCritical?, wrappedContextRecursive?, wrappedContextRegExp?, strictExportPresence?, strictThisContextOnImports? }
       -> Options affecting the normal modules (`NormalModuleFactory`).

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     publicPath: '/'
   },
   module: {
-    loaders: [
+    rules: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader", query: { presets: ['es2015', 'react'] } }
     ]
   },
